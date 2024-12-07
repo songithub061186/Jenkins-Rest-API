dfd93eef21124b6a1e3730cc97eabb41be1353ef
songithub061186 <jersonix@gmai.com>
2024-12-07 11:56:52 +0800

    tryif will work

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
-provider "aws" {
+/*provider "aws" {
   region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/JERSON POGI/.aws/credentials"]
+  shared_credentials_files = ["/Users/PC/.aws/credentials"]
+}*/
+
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = "AKIAYM7POAXJOBGWLV4V"
+  secret_key = "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"
 }
