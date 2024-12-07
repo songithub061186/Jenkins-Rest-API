5228c9a3b13e3ed16451e979ec9f3c7cc9146254
jerson <jersonix@gmail.com>
2024-12-07 12:11:24 +0800

    Rework

--- provider.tf
+++ provider.tf
@@ -1,11 +1,5 @@
-/*provider "aws" {
+provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["/Users/PC/.aws/credentials"]
-}*/
-
-
-provider "aws" {
-  region     = "eu-central-1"
-  access_key = "AKIAYM7POAXJOBGWLV4V"
-  secret_key = "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"
 }
+
